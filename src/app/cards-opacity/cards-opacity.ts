1f719d6e06039affa2a5aebff276409d99a680a5
andiesm813 <86068582+andiesm813@users.noreply.github.com>
2022-11-28 18:00:22 -0300

    Bugs in Samples updates

--- src/app/cards-opacity/cards-opacity.ts
+++ src/app/cards-opacity/cards-opacity.ts
@@ -1,8 +1,9 @@
 import { html, css, LitElement } from 'lit';
-import { customElement } from 'lit/decorators.js';
-import { defineComponents, IgcButtonComponent, IgcCardComponent, IgcRippleComponent } from 'igniteui-webcomponents';
+import { customElement, property } from 'lit/decorators.js';
+import { defineComponents, IgcButtonComponent, IgcCardComponent, IgcIconButtonComponent, IgcIconComponent, IgcRippleComponent } from 'igniteui-webcomponents';
+import TravelAppService from '../service/travel-app-service';
 
-defineComponents(IgcCardComponent, IgcButtonComponent, IgcRippleComponent);
+defineComponents(IgcCardComponent, IgcButtonComponent, IgcRippleComponent, IgcIconButtonComponent, IgcIconComponent);
 
 @customElement('app-cards-opacity')
 export default class CardsOpacity extends LitElement {
@@ -45,8 +46,9 @@ export default class CardsOpacity extends LitElement {
     }
     .group_1 {
       justify-content: flex-start;
-      align-items: center;
+      align-items: stretch;
       align-content: flex-start;
+      gap: 16px;
       position: relative;
       min-width: 50px;
       min-height: 50px;
@@ -78,24 +80,21 @@ export default class CardsOpacity extends LitElement {
       min-width: 50px;
       min-height: 40px;
     }
+    .icon-button::part(base) {
+      color: hsla(var(--ig-secondary-500));
+    }
     .image {
       object-fit: cover;
-      width: 149px;
-      height: 100px;
+      height: 120px;
       min-width: 0;
       min-height: 0;
       flex-shrink: 0;
     }
-    .content {
-      height: max-content;
-      min-width: min-content;
-    }
     .text {
-      color: hsla(var(--ig-success-500));
       height: max-content;
       min-width: min-content;
     }
-    .content_1 {
+    .content {
       color: hsla(var(--ig-surface-500));
       height: max-content;
       min-width: min-content;
@@ -106,134 +105,73 @@ export default class CardsOpacity extends LitElement {
     }
   `;
 
+  constructor() {
+    super();
+    const travelAppService: TravelAppService = new TravelAppService();
+    this.travelAppSelectedArticles = travelAppService.getData('SelectedArticles');
+  }
+
+  @property()
+  private travelAppSelectedArticles?: any[];
+
   render() {
     return html`
       <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'>
       <link href='https://fonts.googleapis.com/css?family=Titillium+Web' rel='stylesheet'>
       <link rel='stylesheet' href='../../ig-theme.css'>
       <div class="row-layout group">
-        <igc-card class="card">
-          <igc-card-header>
-            <h3 slot="title">
-            </h3>
-            <h5 slot="subtitle">
-            </h5>
-          </igc-card-header>
-          <igc-card-content class="body-content">
-            <div class="column-layout group_1">
-              <img src="/src/assets/Logo.svg" class="image" />
-              <p class="typography__subtitle-1 content">
-                Products
-              </p>
-              <h4 class="content">
-                1,236
-              </h4>
-              <p class="typography__subtitle-2 text">
-                Increased 75%
-              </p>
-            </div>
-          </igc-card-content>
-          <igc-card-actions class="actions-content">
-            <igc-button variant="outlined" class="button">
-              VIEW REPORT
-              <igc-ripple></igc-ripple>
-            </igc-button>
-          </igc-card-actions>
-        </igc-card>
-        <igc-card class="card">
-          <igc-card-header>
-            <h3 slot="title">
-            </h3>
-            <h5 slot="subtitle">
-            </h5>
-          </igc-card-header>
-          <igc-card-content class="body-content">
-            <div class="column-layout group_1">
-              <img src="/src/assets/Logo.svg" class="image" />
-              <p class="typography__subtitle-1 content">
-                Products
-              </p>
-              <h4 class="content">
-                1,236
-              </h4>
-              <p class="typography__subtitle-2 text">
-                Increased 75%
-              </p>
-            </div>
-          </igc-card-content>
-          <igc-card-actions class="actions-content">
-            <igc-button variant="outlined" class="button">
-              VIEW REPORT
-              <igc-ripple></igc-ripple>
-            </igc-button>
-          </igc-card-actions>
-        </igc-card>
-        <igc-card class="card">
-          <igc-card-header>
-            <h3 slot="title">
-            </h3>
-            <h5 slot="subtitle">
-            </h5>
-          </igc-card-header>
-          <igc-card-content class="body-content">
-            <div class="column-layout group_1">
-              <img src="/src/assets/Logo.svg" class="image" />
-              <p class="typography__subtitle-1 content">
-                Products
-              </p>
-              <h4 class="content">
-                1,236
-              </h4>
-              <p class="typography__subtitle-2 text">
-                Increased 75%
-              </p>
-            </div>
-          </igc-card-content>
-          <igc-card-actions class="actions-content">
-            <igc-button variant="outlined" class="button">
-              VIEW REPORT
-              <igc-ripple></igc-ripple>
-            </igc-button>
-          </igc-card-actions>
-        </igc-card>
-        <igc-card class="card">
-          <igc-card-header>
-            <h3 slot="title">
-            </h3>
-            <h5 slot="subtitle">
-            </h5>
-          </igc-card-header>
-          <igc-card-content class="body-content">
-            <div class="column-layout group_1">
-              <img src="/src/assets/Logo.svg" class="image" />
-              <p class="typography__subtitle-1 content">
-                Products
-              </p>
-              <h4 class="content">
-                1,236
-              </h4>
-              <p class="typography__subtitle-2 text">
-                Increased 75%
-              </p>
-            </div>
-          </igc-card-content>
-          <igc-card-actions class="actions-content">
-            <igc-button variant="outlined" class="button">
-              VIEW REPORT
-              <igc-ripple></igc-ripple>
-            </igc-button>
-          </igc-card-actions>
-        </igc-card>
+        ${this.travelAppSelectedArticles?.map((item: any) => html`
+          <igc-card class="card">
+            <igc-card-header>
+              <h3 slot="title">
+                Product Name
+              </h3>
+              <h5 slot="subtitle">
+                Department
+              </h5>
+            </igc-card-header>
+            <igc-card-content class="body-content">
+              <div class="column-layout group_1">
+                <img src="${item.image_url}" class="image" />
+                <p class="typography__body-1 text">
+                  Here you can add some description of the product in more details
+                </p>
+                <igc-button variant="outlined" class="button">
+                  Button in Content
+                  <igc-ripple></igc-ripple>
+                </igc-button>
+              </div>
+            </igc-card-content>
+            <igc-card-actions class="actions-content">
+              <igc-button variant="outlined" class="button">
+                Action Btn
+                <igc-ripple></igc-ripple>
+              </igc-button>
+              <igc-icon-button variant="flat" class="icon-button">
+                <span class="material-icons">
+                  favorite
+                </span>
+                <igc-ripple></igc-ripple>
+              </igc-icon-button>
+              <igc-icon-button variant="flat" class="icon-button">
+                <span class="material-icons">
+                  share
+                </span>
+                <igc-ripple></igc-ripple>
+              </igc-icon-button>
+            </igc-card-actions>
+          </igc-card>
+        `)}
       </div>
       <div class="column-layout group_2">
-        <h6 class="content_1">
+        <h6 class="content">
           Issue REPRODUCED IN: Blazor and Web Components generated apps
         </h6>
         <div class="row-layout group_3">
-          <p class="typography__subtitle-1 content_1">
+          <p class="typography__subtitle-1 content">
             Cards have an opacity issue. They look disabled.
           </p>
-          <h6 class="content_1">
+          <h6 class="content">
             REPRODUCED IN: Blazor and WebComponents generated apps
           </h6>
         </div>
